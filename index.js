6a09bf6b1dc16a59255038c8933e92e68841a448
Harshit <harshitgarg545@gmail.com>
2022-12-23 14:10:22 +0530

    final deployment changes

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-// const { getProblemSet } = require("./problemset");
-// const { getHandleData } = require("./handledata");
+const { getProblemSet } = require("./problemset");
+const { getHandleData } = require("./handledata");
 const { compile } = require("morgan");
-const request = require("request");
 
 
 const app = express();
@@ -22,76 +21,6 @@ app.use(function (req, res, next) {
 
 const problemsetData = {};
 const tagList = [];
-
-
-const sendGET = () => {
-    return new Promise((resolve, reject) => {
-        let options = {
-            url: "https://codeforces.com/api/problemset.problems?",
-            method: "GET",
-        };
-
-        request(options, (error, response, body) => {
-            if (error) {
-                console.log("Error details:\n", error);
-                process.exit(0);
-            }
-            const content = JSON.parse(body).result;
-            resolve(content);
-        });
-    });
-};
-
-const getProblemSet = async () => {
-    let data = await sendGET();
-    return data;
-};
-
-const HandleData = (handle) => {
-    return new Promise((resolve, reject) => {
-        options = {
-            url: "https://codeforces.com/api/user.status?handle=" + handle,
-            method: "GET",
-        };
-
-        request(options, (error, response, body) => {
-            if (error) {
-                console.log("user data collection not done\n");
-                console.log("Error details:\n", error);
-            }
-            if (JSON.parse(body).status.toString() !== "OK") {
-                console.log("Invalid CodeForces handle: " + handle);
-                resolve({ error: "Invalid Handle" });
-            }
-            const content = JSON.parse(body).result;
-            resolve(content);
-        });
-    });
-};
-
-const solvedData = async (handle) => {
-    const result = {
-        solved: [],
-    };
-
-    let content = await HandleData(handle);
-    if (content.error) {
-        result.error = content.error;
-        // return result;
-    } else {
-        content.forEach((data) => {
-            if (data.verdict.toString() === "OK")
-                if (result.solved.includes(data.problem.name) === false)
-                    result.solved.push(data.problem.name);
-        });
-    }
-    return result;
-};
-
-const getHandleData = async (handle) => {
-    const result = await solvedData(handle);
-    return result;
-};
 
 const refreshSetData = async () => {
     const data = await getProblemSet();
