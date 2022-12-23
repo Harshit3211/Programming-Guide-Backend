c11aa44aab314ab64a4c57b7752396da8c8a2ac5
Harshit <harshitgarg545@gmail.com>
2022-12-23 13:39:30 +0530

    first commit

--- problemset.js
+++ problemset.js
@@ -1,27 +1,27 @@
-const request = require("request");
-const sendGET = () => {
-    return new Promise((resolve, reject) => {
-        let options = {
-            url: "https://codeforces.com/api/problemset.problems?",
-            method: "GET",
-        };
+// const request = require("request");
+// const sendGET = () => {
+//     return new Promise((resolve, reject) => {
+//         let options = {
+//             url: "https://codeforces.com/api/problemset.problems?",
+//             method: "GET",
+//         };
 
-        request(options, (error, response, body) => {
-            if (error) {
-                console.log("Error details:\n", error);
-                process.exit(0);
-            }
-            const content = JSON.parse(body).result;
-            resolve(content);
-        });
-    });
-};
+//         request(options, (error, response, body) => {
+//             if (error) {
+//                 console.log("Error details:\n", error);
+//                 process.exit(0);
+//             }
+//             const content = JSON.parse(body).result;
+//             resolve(content);
+//         });
+//     });
+// };
 
-const getProblemSet = async () => {
-    let data = await sendGET();
-    return data;
-};
+// const getProblemSet = async () => {
+//     let data = await sendGET();
+//     return data;
+// };
 
-module.exports = {
-    getProblemSet,
-};
+// module.exports = {
+//     getProblemSet,
+// };
